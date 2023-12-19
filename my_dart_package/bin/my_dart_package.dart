9aa35192db5639eb1e3652e0c59bca9deeb34318
fzyzcjy <ch271828n@outlook.com>
2023-12-19 08:43:24 +0800

    feat: empty pkg

---
+++ my_dart_package/bin/my_dart_package.dart
@@ -0,0 +1,5 @@
+import 'package:my_dart_package/my_dart_package.dart' as my_dart_package;
+
+void main(List<String> arguments) {
+  print('Hello world: ${my_dart_package.calculate()}!');
+}
---
+++ my_dart_package/test/my_dart_package_test.dart
@@ -0,0 +1,8 @@
+import 'package:my_dart_package/my_dart_package.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
