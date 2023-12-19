b116b72f21fd9bca8a7fb618f2cf1b9f79be3074
fzyzcjy <ch271828n@outlook.com>
2023-12-19 08:48:05 +0800

    feat: more

--- my_dart_package/lib/my_dart_package.dart
+++ my_dart_package/lib/my_dart_package.dart
@@ -1,3 +1,16 @@
+import 'dart:io';
+
 int calculate() {
+  if(Platform.isWindows) {
+    print('hi windows');
+    return 6 * 7;
+  }
+
+  if(Platform.isMacOS) {
+    print('hi macos');
+    return 6 * 7;
+  }
+
+  print('hi other-os');
   return 6 * 7;
 }
